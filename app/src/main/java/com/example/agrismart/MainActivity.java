90972544386648f32c773bd9390d2a18bd55b86e
FresoloneMarco <m.fresolone2@studenti.unisa.it>
2020-05-24 11:53:02 +0200

    welcome screen

--- app/src/main/java/com/example/agrismart/MainActivity.java
+++ app/src/main/java/com/example/agrismart/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.agrismart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_button = findViewById(R.id.login_button1);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_button.setBackgroundResource(R.drawable.loginbuttonclicked);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
