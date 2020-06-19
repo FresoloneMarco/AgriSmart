802ad0b04888da716a3732d356ccdeefc4dd7507
FresoloneMarco <m.fresolone2@studenti.unisa.it>
2020-06-19 17:26:51 +0200

    aaaaaaa

--- app/src/main/java/com/example/agrismart/fragments/ResocontoMansioneFragment.java
+++ app/src/main/java/com/example/agrismart/fragments/ResocontoMansioneFragment.java
@@ -12,11 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.agrismart.AlertDialog;
+import com.example.agrismart.AlertDialog_CompletaMansione;
 import com.example.agrismart.Concime;
 import com.example.agrismart.MyListAdapter;
 import com.example.agrismart.R;
@@ -37,40 +40,46 @@ public class ResocontoMansioneFragment extends Fragment {
     private ArrayList<Trattamento> trattamenti;
     private ArrayList<Seme> semi;
     private ListView list;
-    private Button addButton;
+    private Button addButton, consegnaButton;
     private EditText qty;
+    private Gson gson;
+    private SharedPreferences.Editor editor;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_completa_mansione, container, false);
+        final View view = inflater.inflate(R.layout.fragment_completa_mansione, container, false);
+        consegnaButton = view.findViewById(R.id.invia_consegna);
 
         /* RETRIEVE DATI DA SHARED PREFERENCES */
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(view.getContext());
-        Gson gson = new Gson();
+        gson = new Gson();
+        editor = sharedPreferences.edit();
         String concimiJson = sharedPreferences.getString("concimi", "");
         String trattamentiJson = sharedPreferences.getString("trattamenti", "");
         String semiJson = sharedPreferences.getString("semi", "");
-        Type typeConcimi = new TypeToken<ArrayList<Concime>>() {}.getType();
-        Type typeSemi = new TypeToken<ArrayList<Seme>>() {}.getType();
-        Type typeTrattamenti = new TypeToken<ArrayList<Trattamento>>() {}.getType();
+        Type typeConcimi = new TypeToken<ArrayList<Concime>>() {
+        }.getType();
+        Type typeSemi = new TypeToken<ArrayList<Seme>>() {
+        }.getType();
+        Type typeTrattamenti = new TypeToken<ArrayList<Trattamento>>() {
+        }.getType();
         concimi = gson.fromJson(concimiJson, typeConcimi);
         trattamenti = gson.fromJson(trattamentiJson, typeTrattamenti);
         semi = gson.fromJson(semiJson, typeSemi);
         /* FINE RETRIEVE */
 
 
-
         spinner_risorse = view.findViewById(R.id.spinner_risorse);
         ArrayList<String> spinnerEntries = new ArrayList<>();
 
-        for(Concime c : concimi){
+        for (Concime c : concimi) {
             spinnerEntries.add(c.getNome());
         }
-        for (Trattamento t : trattamenti){
+        for (Trattamento t : trattamenti) {
             spinnerEntries.add(t.getNome());
         }
-        for(Seme s : semi){
+        for (Seme s : semi) {
             spinnerEntries.add(s.getNome());
         }
 
@@ -92,11 +101,93 @@ public class ResocontoMansioneFragment extends Fragment {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Boolean addable = true;
                 Risorse r = new Risorse(spinner_risorse.getSelectedItem().toString(), qty.getText().toString());
-                risorse.add(r);
+                for (Concime c : concimi) {
+                    if (c.getNome().equals(r.getTitle()) && c.getQty() < Integer.parseInt(r.getQuantity())) {
+                        AlertDialog_CompletaMansione alertDialog = new AlertDialog_CompletaMansione("Errore", "In magazzino sono presenti solo " + String.valueOf(c.getQty()) + " KG di " + c.getNome() + ".", "Ok", "");
+                        alertDialog.show(getFragmentManager(), "example dialog");
+                        addable = false;
+                    }
+                }
+                for (Trattamento t : trattamenti) {
+                    if (t.getNome().equals(r.getTitle()) && t.getQty() < Integer.parseInt(r.getQuantity())) {
+                        AlertDialog_CompletaMansione alertDialog = new AlertDialog_CompletaMansione("Errore", "In magazzino sono presenti solo " + String.valueOf(t.getQty()) + " KG di " + t.getNome() + ".", "Ok", "");
+                        alertDialog.show(getFragmentManager(), "example dialog");
+                        addable = false;
+                    }
+                }
+                for (Seme s : semi) {
+                    if (s.getNome().equals(r.getTitle()) && s.getQty() < Integer.parseInt(r.getQuantity())) {
+                        AlertDialog_CompletaMansione alertDialog = new AlertDialog_CompletaMansione("Errore", "In magazzino sono presenti solo " + String.valueOf(s.getQty()) + " KG di " + s.getNome() + ".", "Ok", "");
+                        alertDialog.show(getFragmentManager(), "example dialog");
+                        addable = false;
+                    }
+                }
+
+                if(addable == true)
+                    risorse.add(r);
+
                 risorseListAdapter.notifyDataSetChanged();
             }
         });
+
+        consegnaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog_CompletaMansione alertDialog = new AlertDialog_CompletaMansione("Attenzione", "Completare la mansione ?", "Si", "No");
+                alertDialog.show(getFragmentManager(), "example dialog");
+                consegna(risorse);
+            }
+        });
+
+
         return view;
     }
+
+    public void consegna(ArrayList<Risorse> risorse) {
+
+        int i = 0;
+        for (Risorse r : risorse) {
+            for (Concime c : concimi) {
+                if (c.getNome().equals(r.getTitle())) {
+                    c.setQty(c.getQty() - Integer.parseInt(r.getQuantity()));
+                    concimi.set(i, c);
+                    String jsonConcimi = gson.toJson(concimi);
+                    editor.putString("concimi", jsonConcimi);
+                    editor.commit();
+                }
+                i++;
+            }
+
+            i = 0;
+            for (Trattamento t : trattamenti) {
+                if (t.getNome().equals(r.getTitle())) {
+                    t.setQty(t.getQty() - Integer.parseInt(r.getQuantity()));
+                    trattamenti.set(i, t);
+                    String jsonTrattamenti = gson.toJson(trattamenti);
+                    editor.putString("trattamenti", jsonTrattamenti);
+                    editor.commit();
+                }
+                i++;
+            }
+
+            i = 0;
+            for (Seme s : semi) {
+                if (s.getNome().equals(r.getTitle())) {
+                    s.setQty(s.getQty() - Integer.parseInt(r.getQuantity()));
+                    semi.set(i, s);
+                    String jsonSemi = gson.toJson(semi);
+                    editor.putString("semi", jsonSemi);
+                    editor.commit();
+                }
+                i++;
+            }
+
+        }
+    }
 }
+
+
+
+
